69d45673a599eed6ffa2e55cd54e0f08b95b56a1
Helio Chissini de Castro <helio@kde.org>
2021-10-11 17:32:51 +0200

    Initial commit

---
+++ hello_boost.cpp
@@ -0,0 +1,17 @@
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/option.hpp>
+#include <iostream>
+
+namespace po = boost::program_options;
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "produce help message")
+        ;
+
+    return 0;
+}
